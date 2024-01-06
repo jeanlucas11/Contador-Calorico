a5bf94e1e1172f96033c1aae3ff8c61c8d462d16
Jean Lucas De Cesare <jean.decesare21@gmail.com>
2024-01-06 09:33:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
 function App() {
+  const [caloriasAdicionadas, setCaloriasAdicionadas] = useState([]);
+  const [totalCalorias, setTotalCalorias] = useState(0);
+  const [caloriasConsumidas, setCaloriasConsumidas] = useState(0);
+
+  const adicionarCalorias = () => {
+    if (caloriasConsumidas > 0) {
+      setCaloriasAdicionadas([...caloriasAdicionadas, caloriasConsumidas]);
+      setTotalCalorias(totalCalorias + caloriasConsumidas);
+      setCaloriasConsumidas(0);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Contador de Calorias</h1>
+      <p>Calorias que serão adicionadas: {caloriasConsumidas}</p>
+      <label>
+        Insira as calorias consumidas:
+        <input
+          type="number"
+          value={caloriasConsumidas}
+          onChange={(e) => setCaloriasConsumidas(parseInt(e.target.value, 10))}
+        />
+      </label>
+      <button onClick={adicionarCalorias}>Adicionar Calorias</button>
+      <p className="total">Total de calorias consumidas: {totalCalorias}</p>
+
+      <hr></hr>
+
+      <div>
+        <h2>Calorias Adicionadas:</h2>
+        <ul>
+          {caloriasAdicionadas.map((caloria, index) => (
+            <li key={index}>{caloria} Calorias</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
